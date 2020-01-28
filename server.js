ca61d13fd20f8d2426c0921021deebc42e972f3f
TheBrellis <brianroyalellis@gmail.com>
2020-01-28 18:13:20 -0500

    reconfig connection to db and sever.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -13,8 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
-
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost/${process.env.DB_NAME}`, { useNewUrlParser: true, useUnifiedTopology: true  }).then(()=>{
+    console.log("Database is connected succesfully");
+});
 
 const routes = require('./routes')
 
